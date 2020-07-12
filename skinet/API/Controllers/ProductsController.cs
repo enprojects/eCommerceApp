15ab154fd6822983e834a6ec87f4745fd9f6f1e0
en <eyaln79@gmail.com>
2020-07-12 18:21:01 +0300

    Starting project

---
+++ skinet/API/Controllers/ProductsController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _ctx;
+
+        public ProductsController(StoreContext ctx)
+        {
+            _ctx = ctx;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _ctx.Products.ToListAsync();
+            return Ok(products);
+        }
+    }
+}
